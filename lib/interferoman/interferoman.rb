19f9e553118466f7fd094e316be0c9285d19d74b
amikula <amikula@gmail.com>
2009-09-16 08:01:47 -0700

    Implemented basic algorithm for guessing

--- lib/interferoman/interferoman.rb
+++ lib/interferoman/interferoman.rb
@@ -9,12 +9,15 @@ module Interferoman
     # Before starting a game, this method will be called to inform the player of all the possible words that may be
     # played.
     def word_list=(list)
+      raise 'reset' unless @list.nil?
+      @list = list.map{|w| w.downcase}
     end
 
     # a new game has started.  The number of guesses the player has left is passed in (default 6),
     # in case you want to keep track of it.
     def new_game(guesses_left)
-      @left = ('a'..'z').to_a
+      @letters = ('a'..'z').to_a
+      @current_game_list = @list.dup
     end
 
     # Each turn your player must make a guess.  The word will be a bunch of underscores, one for each letter in the word.
@@ -22,11 +25,34 @@ module Interferoman
     # the word parameter would be "s___s", if you then guess 'o', the next turn it would be "s_o_s", and so on.
     # guesses_left is how many guesses you have left before your player is hung.
     def guess(word, guesses_left)
-      @left.shift
+      rstring = word.tr('_', '.')
+      regex = Regexp.new("^#{rstring}$")
+      @current_game_list.delete_if{|word| !regex.match(word)}
+
+      counts = [0]*26
+
+      @current_game_list.each do |word|
+        letters = word.scan(/./).uniq
+        letters.each{|l| counts[l[0] - ?a] += 1}
+      end
+
+      guess = ''
+      max_count = 0
+      counts.each_with_index do |count, i|
+        current_letter = (i+?a).chr
+        if @letters.include?(current_letter) && counts[i] > max_count
+          max_count = counts[i]
+          guess = current_letter
+        end
+      end
+
+      @letters.delete(guess)
+      guess
     end
 
     # notifies you that your last guess was incorrect, and passes your guess back to the method
     def incorrect_guess(guess)
+      @current_game_list.delete_if{|w| w.include?(guess.downcase)}
     end
 
     # notifies you that your last guess was correct, and passes your guess back to the method
