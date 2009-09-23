af039385c64ad82b504f598c4e02b38096e44b55
Alf Mikula <amikula@gmail.com>
2009-09-23 07:59:49 -0700

    Added logging for now

--- lib/interferoman/interferoman.rb
+++ lib/interferoman/interferoman.rb
@@ -4,6 +4,8 @@ module Interferoman
     # You may initialize you player but the the initialize method must take NO paramters.
     # The player will only be instantiated once, and will play many games.
     def initialize
+      # TODO Remove logging
+      @outfile = File.new('/Users/alf/tmp/hangman', 'a')
     end
 
     # Before starting a game, this method will be called to inform the player of all the possible words that may be
@@ -22,6 +24,7 @@ module Interferoman
     def new_game(guesses_left)
       @letters = ('a'..'z').to_a
       @current_game_list = nil
+      @guesses_left = guesses_left
     end
 
     # Each turn your player must make a guess.  The word will be a bunch of underscores, one for each letter in the word.
@@ -29,6 +32,7 @@ module Interferoman
     # the word parameter would be "s___s", if you then guess 'o', the next turn it would be "s_o_s", and so on.
     # guesses_left is how many guesses you have left before your player is hung.
     def guess(word, guesses_left)
+      @guesses_left = guesses_left
       if @current_game_list.nil?
         @current_game_list = @list[word.length].dup
       else
@@ -69,11 +73,14 @@ module Interferoman
 
     # you lost the game.  The reason is in the reason parameter
     def fail(reason)
+      @guesses_left -= 1
     end
 
     # The result of the game, it'll be one of 'win', 'loss', or 'fail'.
     # The spelled out word will be provided regardless of the result.
     def game_result(result, word)
+      @outfile << "Result: #{result}, guesses left: #{@guesses_left}, word: #{word}\n"
+      @outfile.flush
     end
   end
 end
