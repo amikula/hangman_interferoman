141ede83d661f63ec297551678df9ef1f621aab3
Alf Mikula <amikula@gmail.com>
2009-09-23 06:11:07 -0700

    Optimization: Divide word list by length at assignment time

--- spec/interferoman/interferoman_spec.rb
+++ spec/interferoman/interferoman_spec.rb
@@ -2,11 +2,51 @@ require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 require 'interferoman/interferoman'
 
 describe Interferoman::Interferoman do
-
-  it "should be instantiable with no paramters" do
-
-    lambda { Interferoman::Interferoman.new }.should_not raise_error
-
+  describe :initialize do
+    it "should be instantiable with no parameters" do
+      lambda { Interferoman::Interferoman.new }.should_not raise_error
+    end
   end
 
+  describe :word_list= do
+    it 'divides the words into sub-lists by word length' do
+      subject.word_list = %w{a b cd efg hijkl}
+
+      list = subject.instance_variable_get("@list")
+
+      list[1].sort.should == %w{a b}
+      list[2].should == %w{cd}
+      list[3].should == %w{efg}
+      (list[4] == nil || list[4].length == 0).should be_true
+      list[5].should == %w{hijkl}
+    end
+  end
+
+  describe :guess do
+    before do
+      subject.new_game(6)
+      subject.word_list = %w{a b cd ef}
+    end
+
+    it 'guesses a letter based on the length of the word' do
+      guess = subject.guess('..', 6)
+
+      guess.should_not be_nil
+      %w{c d e f}.should include(guess)
+    end
+
+    it 'filters out words that do not match the current word state' do
+      subject.guess('__', 6)
+      guess = subject.guess('_f', 6)
+
+      guess.should == 'e'
+    end
+
+    it 'does not overwrite the original list' do
+      subject.guess('__', 6)
+      guess = subject.guess('_f', 6)
+
+      subject.instance_variable_get('@list')[2].sort.should == %w{cd ef}
+    end
+  end
 end
